940e35ddc2fb67e26353f716db56d1f5c46d5fd4
kizitocedric <kizitocedric4@gmail.com>
2026-09-05 16:30:59 +0300

    Update initial.java

--- initial.java
+++ initial.java
@@ -2,3 +2,15 @@ This is my intial
 
  //cedi
  java file for the first java file i have written!
+
+
+ git clone "https://github.com/kizitocedric/first-java.git
+ you cd into the folder and you type code . to work with vscode
+ you open vscode and
+ create branch ie   git branch cedi or calvin or dev
+ Then select or checkout by git checkout cedi
+ After finishing your work you add to you repo ie git add . the . mean all the changes
+ After adding changes you commit by git commit -m "i have made these changes or a message readable to the workmates
+ After you push changes to the repo ie git push or push to an upstream
+ After you merge to the main repository on github
+ Then you pull news from main
